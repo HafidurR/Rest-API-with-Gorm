22db317b75dcb76d6888cdfab121cdb01859163d
HafidurR <deifah126@gmail.com>
2023-01-07 20:27:35 +0700

    update dikit

--- main.go
+++ main.go
@@ -5,19 +5,6 @@ import (
 	"api-gorm/routes"
 )
 
-
-type Product struct {
-	ID    int             `form:"id" json:"id"`
-	Name  string          `form:"name" json:"name"`
-	Description  string 	`form:"code" json:"description"`
-	Stock       int    		`json:"stock"`
-}
-type Result struct {
-	Code    int         `json:"code"`
-	Data    interface{} `json:"data"`
-	Message string      `json:"message"`
-}
-
 func main() {
 	config.Connect()
 	routes.HandleRequests()
